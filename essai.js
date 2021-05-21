9f3fe6d7c0e96ebff6c175570808ad7f89559000
Hayat NAIRI <hayat.nairi.nairi@gmail.com>
2021-05-21 18:47:29 +0200

    mon fichier

---
+++ essai.js
@@ -0,0 +1,147 @@
+var maillots =  [
+    { 
+        id : 1, // clé : valeur
+        nom : "Maillot Blanc", 
+        couleur: "Blanc",
+        description : "Maillot uni blanc",
+        quantite: 2,
+        prix : 35,
+        image : "asset/img/blanc.jpg",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 2, 
+        nom : "Maillot Or", 
+        couleur: "Or",
+        description : "Maillot uni or",
+        quantite: 4,
+        prix : 35,
+        image : "asset/img/or.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 3, 
+        nom : "Maillot Bleu", 
+        couleur: "Bleu",
+        description : "Maillot uni bleu",
+        quantite: 6,
+        prix : 35,
+        image : "asset/img/bleu.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 4, 
+        nom : "Maillot Turquoise", 
+        couleur: "Turquoise",
+        description : "Maillot uni turquoise",
+        quantite: 8,
+        prix : 35,
+        image : "asset/img/turquoise.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 5, 
+        nom : "Maillot Jaune", 
+        couleur: "Jaune",
+        description : "Maillot uni jaune",
+        quantite: 1,
+        prix : 35,
+        image : "asset/img/jaune.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 6, 
+        nom : "Maillot Violet", 
+        couleur: "Violet",
+        description : "Maillot uni violet",
+        quantite: 8,
+        prix : 35,
+        image : "asset/img/violet.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 7, 
+        nom : "Maillot Noir", 
+        couleur: "Noir",
+        description : "Maillot uni noir",
+        quantite: 3,
+        prix : 35,
+        image : "asset/img/noir.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 8, 
+        nom : "Maillot Rouge", 
+        couleur: "Rouge",
+        description : "Maillot uni rouge",
+        quantite: 9,
+        prix : 35,
+        image : "asset/img/rouge.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 9, 
+        nom : "Maillot Vert", 
+        couleur: "Vert",
+        description : "Maillot uni vert",
+        quantite: 2,
+        prix : 35,
+        image : "asset/img/vert.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    
+    { 
+        id : 10, 
+        nom : "Maillot Usa", 
+        couleur: "Usa",
+        description : "Maillot uni usa",
+        quantite: 7,
+        prix : 35,
+        image : "asset/img/usa.jpg",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 11, 
+        nom : "Maillot Orange", 
+        couleur: "Orange",
+        description : "Maillot uni orange",
+        quantite: 1,
+        prix : 35,
+        image : "asset/img/orange.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+    { 
+        id : 12, 
+        nom : "Maillot Blanc cassé", 
+        couleur: "Blanc cassé",
+        description : "Maillot uni blanc cassé",
+        quantite: 5,
+        prix : 35,
+        image : "asset/img/blanc.png",
+        image_hover : "asset/img/blanc-benzema.png",
+    
+    },
+
+];
